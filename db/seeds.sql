8e800f30c8de8e33a7878b4ab3cf94ffde92e327
Andrea Boyd <andreanboyd@yahoo.com>
2020-12-11 20:37:49 -0600

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Nashvile Hot", true),("Veggie", false),("Whiskey", true),("Whopper",false),("Big Mac",false),("Big River", false);
+
